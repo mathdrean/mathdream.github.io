e4c02eb4c040d32a5246404ed31a78aab6b4316a
mathdream <87427315+mathtime@users.noreply.github.com>
2021-07-16 20:48:04 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // 你的所有代码都应该写在这里
-  alert("Hello JavaScript");
+  let targetImage = document.querySelector("#smart-image");
+  targetImage.addEventListener("click", function() {
+    targetImage.classList.remove("small");
+});
+    
+    
+  
 });
